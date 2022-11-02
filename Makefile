a5ab61314028cde12a49ee97a9dd949075f5728a
csjiet <csjiet@gmail.com>
2022-11-02 18:11:21 -0500

    updated psort file buffer implementation

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 run: compile
-	./psort
+	./psort input.bin
+
+runR: compileR
+	./psort input.bin
 
 compile:
 	gcc -Wall -Werror -pthread -O psort.c -o psort  
 
+compileR:
+	gcc -Wall -pthread -O psort.c -o psort  
+
 clean:
 	rm -rf ./*.o
-	rm ./psort 
 
--- psort.c
+++ psort.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define RECORD_SIZE 100
+
+// Global variable
+char* buf;
 
 int main(int argc, char *argv[]) {
-struct stat st;
-int size;
-stat("output.bin", &st);
-size = st.st_size;
-unsigned char *buffer = malloc(100*sizeof(char));
-FILE *fp;
-fp = fopen("output.bin", "rb");
-fread(buffer, sizeof(buffer), 1, fp);
-for (int i = 0; i < 100; i++) {
-    printf("%x ", buffer[i]);
-}
+    struct stat st;
+    int size;
+    
+    // Open a file
+    int fd = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR);  
+
+    // Retrieves size of input file
+    if(fstat(fd, &st) == -1)
+        perror("Couldn't get file size\n");
+
+    size = st.st_size;
+
+    // Map the input file into a buffer
+    buf = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    // Each record can be accessed in 100 byte strides: index*100. 
+    // Each record's first 4 bytes can be accessed by reading range: index*100 to (index*100)+4
+
+    // Loops through each character in the binary file
+    /*
+    for(int i = 0; i< st.st_size; i++){
+        printf("%c", buf[i]);
+    }
+    */
+
     return 0;
 }
