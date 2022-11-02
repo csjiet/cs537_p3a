dd7135ab7bc71585e3851386c7af08c27cd068fa
Collin Lull <lull@royal-18.cs.wisc.edu>
2022-11-02 15:28:48 -0500

    Started reading in bin file

--- psort.c
+++ psort.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
+    unsigned char buffer[100];
+    FILE *fp;
+    fp = fopen("output.bin", "rb");
+    fread(buffer, sizeof(buffer), 1, fp);
+    for (int i = 0; i < 100; i++) {
+        printf("%x ", buffer[i]);
+    }
     return 0;
 }
