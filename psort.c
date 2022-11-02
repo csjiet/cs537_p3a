62f3d3b440e4807d6aac8a9c96c66d76e7f14c42
Collin Lull <lull@royal-18.cs.wisc.edu>
2022-11-02 16:11:02 -0500

    Updated size of file descriptor

--- psort.c
+++ psort.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
-    unsigned char *buffer = malloc(100*sizeof(char));
-    FILE *fp;
-    fp = fopen("output.bin", "rb");
-    fread(buffer, sizeof(buffer), 1, fp);
-    for (int i = 0; i < 100; i++) {
-        printf("%x ", buffer[i]);
-    }
+struct stat st;
+int size;
+stat("output.bin", &st);
+size = st.st_size;
+unsigned char *buffer = malloc(100*sizeof(char));
+FILE *fp;
+fp = fopen("output.bin", "rb");
+fread(buffer, sizeof(buffer), 1, fp);
+for (int i = 0; i < 100; i++) {
+    printf("%x ", buffer[i]);
+}
     return 0;
 }
